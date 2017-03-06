51fe0a83080467c50c8822007b0fe14d86369271
MURAMATSU Atsushi <amura@tomato.sakura.ne.jp>
2017-03-06 20:13:54 +0900

    version bump to 0.2.1

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -15,7 +15,11 @@ class Plugin extends gitbucket.core.plugin.Plugin {
   override val pluginId: String = "rst"
   override val pluginName: String = "ReSTructured text Plugin"
   override val description: String = "Provides ReSTructured text rendering for GitBucket."
-  override val versions: List[Version] = List(new Version("0.2.0.99"))
+  override val versions: List[Version] = List(
+    new Version("0.1.0"),
+    new Version("0.2.0"),
+    new Version("0.2.1")
+  )
 
   private[this] var renderer: Option[RstRenderer] = None
 
